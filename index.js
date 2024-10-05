f148dee232b372bae9d6c1aacb5ca36a64928d0d
AnaCarolinamoreiradesiqueira <carolina.trabalho94@gmail.com>
2024-10-04 22:03:36 -0300

    atualizaçao

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log ('teste')
+import http from 'http'
+
+const PORT = 3300;
+
+const rotas={
+    '/': 'PRIMEIRA ROTA',
+    '/ROTA2': 'ROTA SEGUNDARIA',
+    '/ROTA3': 'ROTA DE TESTE'
+}
+
+const teste = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end(rotas[req.url])
+})
+
+teste.listen(PORT, ()=>{
+    console.log('FUNCIONOU PORRA')
+})
